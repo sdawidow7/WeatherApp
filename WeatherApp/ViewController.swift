ca72060518c041238b96c6853e89f4c1e0b978b6
SzymonDawidow <szymon.dawidow@miquido.com>
2023-08-03 12:37:56 +0200

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Szymon Dawidów on 03/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
