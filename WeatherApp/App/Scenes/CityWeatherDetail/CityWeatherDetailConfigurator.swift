c25c934f8b8260536ce4c60d53ec2f8f87390607
SzymonDawidow <szymon.dawidow@miquido.com>
2023-08-07 09:29:52 +0200

    Show empty weather detail

--- WeatherApp/App/Scenes/CityWeatherDetail/CityWeatherDetailConfigurator.swift
+++ WeatherApp/App/Scenes/CityWeatherDetail/CityWeatherDetailConfigurator.swift
@@ -1,4 +1,8 @@
-final class CityWeatherDetailConfigurator {
+protocol CityWeatherDetailConfigurator {
+    func defaultScene(with cityModelId: CityModelId) -> CityWeatherDetailViewController
+}
+
+final class CityWeatherDetailConfiguratorImpl: CityWeatherDetailConfigurator {
     func defaultScene(with cityModelId: CityModelId) -> CityWeatherDetailViewController {
         let viewModel = CityWeatherDetailViewModelImpl(cityModelId: cityModelId)
         return CityWeatherDetailViewController(viewModel: viewModel)
--- WeatherApp/App/Scenes/SelectCity/SelectCityRouter.swift
+++ WeatherApp/App/Scenes/SelectCity/SelectCityRouter.swift
@@ -1,9 +1,21 @@
+import UIKit
+
 protocol SelectCityRouter {
     func showCityDetails(for cityId: CityModelId)
 }
 
 final class SelectCityRouterImpl: SelectCityRouter {
+
+    private let weatherDetailConfigurator: CityWeatherDetailConfigurator
+
+    init(weatherDetailConfigurator: CityWeatherDetailConfigurator) {
+        self.weatherDetailConfigurator = weatherDetailConfigurator
+    }
+
+    weak var viewController: UIViewController?
     func showCityDetails(for cityId: CityModelId) {
+        let weatherDetailViewController = weatherDetailConfigurator.defaultScene(with: cityId)
+        viewController?.navigationController?.show(weatherDetailViewController, sender: nil)
         print("NAVIGATE TO CITY: \(cityId)") // TODO: Implement
     }
 }
